ae17866eb7e6c737635c43aa7c0c594d5f804887
Gabriela Vital <gabisinha0107@hotmail.com>
2015-09-21 11:09:36 -0300

    implementar parte gráfica

---
+++ Tela.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+
+public class Tela extends JFrame implements ActionListener{
+  private static final long serialVersionUID = 1L;
+
+  public Tela(){
+    super("Conversor de Bases");
+  }
+  public void actionPerformed(ActionEvent evento){
+	  
+  }
+
+}
