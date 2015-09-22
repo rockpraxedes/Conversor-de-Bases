282fae395c8a3726d6e78990f9cd381b16525c7f
Gabriela Vital <gabisinha0107@hotmail.com>
2015-09-22 20:47:45 -0300

    tirando o try/catch que nao servia pra lixo nenhum

--- Conversor.java
+++ Conversor.java
@@ -17,32 +17,26 @@ public class Conversor {
 		fracionario = Math.round((numDecimal - (int)numDecimal)*100);
 		fracionario /= 100;
 		
-		try{
-			if (numDecimal == 0){
-				return "0"; //se o numero a ser convertido for = 0, o resultado é 0 né haha
-			}else{
-			        while(inteiro > 0) { //convertendo parte inteira do número
-			        	resultadoBinarioInteiro = (inteiro % base) + resultadoBinarioInteiro;
-			            inteiro /= base;
-			        }
-			        
-			        if (fracionario != 0 && fracionario > 0){
-			        	while (fracionario > 0){ //verificando se o numero nao é inteiro
-			        		// multiplica 0.XX, pega a parte inteira do resultado
-			        		// ARRUMEI ESSE CARALHO!! UFA
-			        		fracionario *= base;
-			        		inteiroFrac = (int)fracionario;
-			        		resultadoBinFrac = inteiroFrac + resultadoBinFrac;
-			        		fracionario = Math.round((fracionario - (int)fracionario)*100);
-			        		fracionario /= 100;
-			        	}
-			        }
+		if (numDecimal == 0 && base > 0){
+			return "0"; //se o numero a ser convertido for = 0, o resultado é 0 né haha
+		}else if (base > 0){
+			while(inteiro > 0) { //convertendo parte inteira do número
+				resultadoBinarioInteiro = (inteiro % base) + resultadoBinarioInteiro;
+				inteiro /= base;
 			}
-		}
-		catch (Exception e){
-			if (base == 0 || base < 0){ //vai que o engraçadinho coloca base 0 pra testar né ahaha
-				System.out.println(e.getMessage() + "/n Base digitada é inválida. Favor escolher uma base positiva.");
+			if (fracionario != 0 && fracionario > 0){
+				while (fracionario > 0){ //verificando se o numero nao é inteiro
+					// multiplica 0.XX, pega a parte inteira do resultado
+					// ARRUMEI ESSE CARALHO!! UFA
+					fracionario *= base;
+					inteiroFrac = (int)fracionario;
+					resultadoBinFrac = inteiroFrac + resultadoBinFrac;
+					fracionario = Math.round((fracionario - (int)fracionario)*100);
+					fracionario /= 100;
+				}
 			}
+		}else{
+			System.out.println("Base digitada é inválida. Favor escolher uma base positiva.");
 		}
 		return resultadoBinarioInteiro + resultadoBinFrac; // ta certo esse retorno aqui? nao tenho certeza
 	}
